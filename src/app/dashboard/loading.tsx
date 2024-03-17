f942494aff06d513456a2f76fa0c95dc5556c224
feerdaus <firdauslawal@gmail.com>
2024-03-17 11:45:49 +0100

    Update loading UI

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="h-full flex items-center justify-center">
       <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-primary"></div>
     </div>
   );
