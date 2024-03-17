00f878c527beea371643539cec84da0b78eb2d9c
feerdaus <firdauslawal@gmail.com>
2024-03-17 11:48:16 +0100

    Add top nav

---
+++ src/components/TopNav.tsx
@@ -0,0 +1,34 @@
+import { auth } from "@/auth";
+import { Routes } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
+const TopNav = async () => {
+  const session = await auth();
+  return (
+    <nav className="px-6 py-4">
+      <div className="flex justify-between items-center">
+        <Image src="/logo.png" alt="Spend wise logo" width={100} height={100} />
+
+        <div className="">
+          {session ? (
+            <Link
+              href={Routes.dashboard.fullPath}
+              className="text-lg font-medium"
+            >
+              <button className="btn btn-primary-outlined">
+                Go to dashboard
+              </button>
+            </Link>
+          ) : (
+            <Link href={Routes.signIn.fullPath}>
+              <button className="btn btn-primary">Sign in</button>
+            </Link>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default TopNav;
--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,2 +1,5 @@
 export const Hero_Img =
   "https://ik.imagekit.io/7wpxe2myx/firdaus/hero.png?updatedAt=1709908598734";
+
+export const Logo =
+  "https://ik.imagekit.io/4woq8zlkh/expense-tracker/expense-tracker-logo_DXDdksWsq.png?updatedAt=1710668203470";
