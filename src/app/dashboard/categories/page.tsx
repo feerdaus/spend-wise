535347f50c42d29bceb42d47f14b9936d5e179ac
feerdaus <firdauslawal@gmail.com>
2024-03-17 13:47:52 +0100

    Update category card

--- src/app/dashboard/categories/page.tsx
+++ src/app/dashboard/categories/page.tsx
@@ -1,13 +1,45 @@
+import CategoryCard from "@/components/CategoryCard";
 import { Routes } from "@/constants";
 import { db } from "@/db";
 import Link from "next/link";
+
+// const  categories = {
+//   allocatedamount:35346
+// }
 
 export default async function CategoriesPage() {
   const allCategories = await db.expenseCategory.findMany();
   console.log(allCategories);
   return (
     <div>
-      <h2 className="font-semibold text-xl mb-4">All Categories</h2>
+      <h2 className="font-medium text-xl">All Categories</h2>
+      <hr className="my-8" />
+      <section className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div className="col-span-1">
+          <CategoryCard
+            category="Shopping"
+            allocatedAmount={5678}
+            balance={34634}
+            expense={4244}
+          />
+        </div>
+        <div className="col-span-1">
+          <CategoryCard
+            category="Shopping"
+            allocatedAmount={5678}
+            balance={34634}
+            expense={4244}
+          />
+        </div>
+        <div className="col-span-1">
+          <CategoryCard
+            category="Shopping"
+            allocatedAmount={5678}
+            balance={34634}
+            expense={4244}
+          />
+        </div>
+      </section>
       <Link href={Routes.newCategories.fullPath} className="btn btn-primary">
         New Category
       </Link>
--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,7 +1,46 @@
-export const CategoryCard: React.FC = () => {
+import React from "react";
+
+interface CategoryCardProps {
+  category: string;
+  expense: number;
+  allocatedAmount: number;
+  balance: number;
+}
+
+const CategoryCard: React.FC<CategoryCardProps> = ({
+  allocatedAmount,
+  balance,
+  expense,
+  category,
+}) => {
   return (
-    <div>
-      <h1>CategoryCard</h1>
+    <div className="rounded-lg dark-bg bx-shadow px-4 py-5 max-w-md text-white">
+      <div className="flex justify-between">
+        <h3 className="text-2xl font-semibold ">{category}</h3>
+        <div>
+          <p className="font-medium">Balance left</p>
+          <p className="font-medium text-3xl">
+            $ {Number(balance).toLocaleString()}
+          </p>
+        </div>
+      </div>
+
+      <div className="flex justify-between mt-11">
+        <div>
+          <p className="text-lg">Allocated amount</p>
+          <p className="font-medium text-2xl">
+            {Number(allocatedAmount).toLocaleString()}
+          </p>
+        </div>
+        <div>
+          <p className="text-lg">Expenses</p>
+          <p className="font-semibold text-2xl">
+            $ {Number(expense).toLocaleString()}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
+
+export default CategoryCard;
